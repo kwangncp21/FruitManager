8a06056a7346571577598740dd4405117d4a6f90
kwangncp21 <157690526+kwangncp21@users.noreply.github.com>
2025-07-22 00:26:27 +0700

    ig

---
+++ Backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+//Load env vars
+dotenv.config({path:'./config/config.env'});
+
+connectDB();
+
+const app = express();
+
+// app.use(cors({
+//   origin: 'http://localhost:5500', // เปลี่ยนตาม frontend ที่ใช้
+//   credentials: true
+// }));
+app.use(cors({
+  origin: 'http://localhost:3001', // เปลี่ยนตาม frontend ที่ใช้
+  credentials: true
+}));
+
+//Body parser
+app.use(express.json());
+
+app.use(cookieParser());
+
+const auth = require('./routes/auth');
+const csvRoute = require('./routes/csv');
+
+//Routing
+app.use(`/api/v1/auth`,auth);
+app.use('/api/v1/csv', csvRoute);
+
+
+const PORT=process.env.PORT || 3000;
+app.listen(PORT, console.log('server running in',process.env.NODE_ENV,'mode on port',PORT));
