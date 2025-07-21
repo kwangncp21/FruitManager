7e90396c40ad0f8f36f7bf557e4102ff4af92496
kwangncp21 <157690526+kwangncp21@users.noreply.github.com>
2025-07-22 00:11:07 +0700

    modified

--- Frontend/login-ui/src/App.tsx
+++ Frontend/login-ui/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} /> // เพิ่มภายหลังได้
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
