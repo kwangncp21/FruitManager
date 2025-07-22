fadce62737bc49a8af90d817f90bf9307f12669d
kwangncp21 <157690526+kwangncp21@users.noreply.github.com>
2025-07-22 15:36:29 +0700

    modified

--- Frontend/login-ui/src/pages/Home.tsx
+++ Frontend/login-ui/src/pages/Home.tsx
@@ -80,10 +80,29 @@ const Home: React.FC = () => {
       console.error('Update failed:', err);
     }
   };
+  const handleLogout = async () => {
+  try {
+    await axios.get('http://localhost:3000/api/v1/auth/logout', { withCredentials: true });
+    window.location.href = '/'; // หรือเปลี่ยนเป็น '/login' ถ้าคุณมีหน้า login
+  } catch (err) {
+    console.error('Logout failed:', err);
+  }
+};
+
+
 
 return (
+
     <div className="p-6 max-w-4xl mx-auto">
       <h1 className="text-2xl font-bold mb-4">🍍 Fruit Data</h1>
+      <div className="flex justify-end mb-4">
+        <button
+            onClick={handleLogout}
+            className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
+        >
+            Logout
+        </button>
+    </div>
 
       <div className="flex items-center gap-4 mb-6">
         <input type="file" onChange={e => setFile(e.target.files?.[0] || null)} />
